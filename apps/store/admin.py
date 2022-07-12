ff0a018b9c46669bf2b6689d3b00a40d2e7112c7
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-07-12 14:52:17 +0000

    config: product model

--- apps/store/admin.py
+++ apps/store/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category
+from .models import Category, Product
 
 
 # registers my models with the django admin so I can use them.
 admin.site.register(Category)
-
+admin.site.register(Product)
