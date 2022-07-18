da1e62ff54242e5f4f7e4a8564cf100331d7dd7f
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-07-18 14:07:06 +0000

    config: view product URL

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path
 
 from apps.core.views import home, contact
+from apps.store.views import view_product
 
 # url configarations for apps
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('Contact_Us/', contact, name='contact'),
+    path('<slug:slug>/View_Details/',view_product, name='view_product'),
 ]
