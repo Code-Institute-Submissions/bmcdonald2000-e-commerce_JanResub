32b0b1dbf00e85d6849139c22b2e51330cd9b908
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-07-18 16:20:05 +0000

    add: category details views

--- apps/store/views.py
+++ apps/store/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 
 
 # producdetailsview.
 
-def view_product(request, slug):
+def view_product(request, category_slug, slug):
     product = get_object_or_404(Product, slug=slug)
 
     context = {
@@ -12,3 +12,14 @@ def view_product(request, slug):
     }
 
     return render(request, 'view_product.html', context)
+
+
+def category_details(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    products = category.products.all()
+    context = {
+        'category': category,
+        'products': products
+    }
+
+    return render(request, 'category_details.html', context)
