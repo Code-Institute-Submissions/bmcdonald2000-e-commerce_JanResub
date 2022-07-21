e683083dd36b9cd73f83e1c3b71bc76c4899928d
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-07-21 19:09:30 +0000

    add: delete products from cart api function

--- apps/store/api.py
+++ apps/store/api.py
@@ -1,16 +1,18 @@
+import json
+from .models import Product
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+
 from apps.cart.cart import Cart
 
-from .models import Product
 
-
-# function to add items to cart
+#  add items api function
 def add_to_cart(request):
+    data = json.loads(request.body)
     jsonresponse = {'success': True}
-    product_id = request.Post.get('product_id')
-    update = request.POST.get('update')
-    quantity = request.POST.get('quantity', 1)
+    product_id = data['product_id']
+    update = data['update']
+    quantity = data['quantity']
 
     cart = Cart(request)
 
@@ -20,5 +22,17 @@ def add_to_cart(request):
         cart.add(product=product, quantity=1, update_quantity=False)
     else:
         cart.add(product=product, quantity=quantity, update_quantity=True)
+
+    return JsonResponse(jsonresponse)
+
+
+# delete items api function
+def delete_from_cart(request):
+    data = json.loads(request.body)
+    jsonresponse = {'success': True}
+    product_id = str(data['product_id'])
+
+    cart = Cart(request)
+    cart.delete(product_id)
 
     return JsonResponse(jsonresponse)
