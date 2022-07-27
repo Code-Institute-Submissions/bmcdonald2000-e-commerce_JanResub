55a5182f2523a5b39be4c77f16034fe723ebfc0c
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-07-27 10:12:20 +0000

    feat: product image

--- apps/store/models.py
+++ apps/store/models.py
@@ -1,3 +1,6 @@
+from io import BytesIO
+from django.core.files import File
+from PIL import Image
 from django.db import models
 
 
@@ -25,6 +28,8 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
     featured = models.BooleanField(default=False)
+    image = models.ImageField(upload_to="media/images/", blank=True, null=True)
+    thumbnail = models.ImageField(upload_to="media/images/", blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
     # orders products by the date they were added
@@ -34,3 +39,20 @@ class Product(models.Model):
     # Product names are displayed in the admin
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.thumbnail = self.thumbnails(self.image)
+
+        super().save(*args, **kwargs)
+
+    # create thumbnails
+    def thumbnails(self, image, size=(300, 200)):
+
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+        thumbnail = File(thumb_io, name=image.name)
+        return thumbnail
