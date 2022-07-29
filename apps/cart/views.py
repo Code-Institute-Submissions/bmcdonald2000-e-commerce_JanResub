e9c8fc7db862d87da67f9adba37cd614110110a7
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-07-29 10:54:37 +0000

    add: product details

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -5,11 +5,19 @@ from .cart import Cart
 
 # function to add item to cart
 def cart(request):
-
     cart_details = Cart(request)
+    productstring = ''
+
+    for item in cart_details:
+        product = item['product']
+
+        x = "{'id': '%s', 'title': '%s', 'price': '%s', 'quantity': '%s', 'total': '%s'}," % (product.id, product.title, product.price, item['quantity'], item['total'])
+
+        productstring = productstring + x
 
     context = {
-        'cart_details': cart_details
+        'cart_details': cart_details,
+        'productstring': productstring
     }
 
     return render(request, 'cart.html', context)
