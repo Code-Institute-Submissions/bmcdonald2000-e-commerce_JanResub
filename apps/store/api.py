65c33690770a9d6ba8b72565f1ae0bf06f4207b6
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-07-30 10:12:00 +0000

    feat: checkout

--- apps/store/api.py
+++ apps/store/api.py
@@ -1,9 +1,41 @@
 import json
 from .models import Product
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 
 from apps.cart.cart import Cart
+from apps.order.utils import checkout
+from apps.order.models import Order, Item
+
+
+# checkout api function
+def Checkout(request):
+    data = json.loads(request.body)
+    jsonresponse = {'success': True}
+    data = json.loads(request.body)
+    jsonresponse = {'success': True}
+    first_name = data['first_name']
+    last_name = data['last_name']
+    email = data['email']
+    address = data['address']
+    postcode = data['postcode']
+    city = data['city']
+
+    cart = Cart(request)
+
+    orderid = checkout(request, first_name, last_name, email, address, postcode, city)
+
+    paid = True
+
+    if paid == True:
+        order = Order.objects.get(pk=orderid)
+        order.paid = True
+        order.amount_paid = cart.total_cost()
+        order.save()
+
+        cart.clear()
+
+    return JsonResponse(jsonresponse)
 
 
 #  add items api function
