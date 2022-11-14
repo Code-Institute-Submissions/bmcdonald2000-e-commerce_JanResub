aacb5106ec7fc94e67472b2a1633eb245e491e65
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-11-14 17:41:30 +0000

    add: coupon functionality to the store and tidy up the code

--- apps/store/api.py
+++ apps/store/api.py
@@ -1,24 +1,49 @@
 import json
+import stripe
 from .models import Product
-from django.http import JsonResponse
+from django.core.mail import EmailMultiAlternatives
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import get_object_or_404, redirect
+from django.template.loader import render_to_string
+from apps.order.views import render_to_pdf
 from django. conf import settings
 
 from apps.cart.cart import Cart
 from apps.order.utils import checkout
-from apps.order.models import Order, Item
+from apps.order.models import Order
+from apps.coupon.models import Coupon
+from .utilities import decrement_product_quantity, send_order_confirmation
 
 
 # api function to create a checkout session
 def checkout_session(request):
-    cart = Cart()
 
+    data = json.loads(request.body)
+
+    coupon_code = data['coupon_code']
+    coupon_value = 0
+
+    # Coupon code
+    if coupon_code != '':
+        coupon = Coupon.objects.get(code=coupon_code)
+
+        if coupon.can_use():
+            coupon_value = coupon.value
+            coupon.use()
+
+    cart = Cart(request)
+    # retrives hidden stripe variable
     stripe.api_key = settings.STRIPE_HIDDEN
 
     items = []
-
+    # calculates the price before and after a coupon is applied.
     for item in cart:
         product = item['product']
+
+        price = int(product.price * 100)
+
+        if coupon_value > 0:
+            price = int(price - (price * (int(coupon_value) / 100)))
 
         obj = {
             'price_data': {
@@ -26,32 +51,58 @@ def checkout_session(request):
                 'product_data': {
                     'name': product.title
                 },
-                'unit_amount': int(product.price * 100)
+                'unit_amount': price
             },
             'quantity': item['quantity']
         }
 
         items.append(obj)
-    
+
+    # creates a session for the stripe checkout
     session = stripe.checkout.Session.create(
             payment_method_types=['card'],
             line_items=items,
             mode='payment',
-            success_url='http://127.0.0.1:8000/cart/success/',
-            cancel_url='http://127.0.0.1:8000/cart/'
+            success_url='https://8000-bmcdonald2000-ecommerce-vwm03kplm3y.ws-eu74.gitpod.io/cart/success/',
+            cancel_url='https://8000-bmcdonald2000-ecommerce-vwm03kplm3y.ws-eu74.gitpod.io/cart/'
     )
+
+    # creates an order
+    first_name = data['first_name']
+    last_name = data['last_name']
+    email = data['email']
+    address = data['address']
+    postcode = data['postcode']
+    city = data['city']
+    phone = data['phone']
+    payment_intent = session.payment_intent
+
+    orderid = checkout(request, data['first_name'], data['last_name'], data['email'], data['address'], data['postcode'], data['city'], data['phone'])
+
+    total = 0.00
+
+    # when an Item is in the cart the data is saved until the session expires
+
+    for item in cart:
+        product = item['product']
+        total = total + (float(product.price) * int(item['quantity']))
+
+    if coupon_value > 0:
+        price = price * (coupon_value / 100)
+
+    order = Order.objects.get(pk=orderid)
+    order.payment_intent = payment_intent
+    order.paid_amount = total
+    order.used_coupon = coupon_code
+    order.save()
 
     return JsonResponse({'session': session})
 
 
-
-
-
-
 # checkout api function
 def Checkout(request):
-    data = json.loads(request.body)
-    jsonresponse = {'success': True}
+    cart = Cart(request)
+
     data = json.loads(request.body)
     jsonresponse = {'success': True}
     first_name = data['first_name']
@@ -60,8 +111,6 @@ def Checkout(request):
     address = data['address']
     postcode = data['postcode']
     city = data['city']
-
-    cart = Cart(request)
 
     orderid = checkout(request, first_name, last_name, email, address, postcode, city)
 
@@ -72,6 +121,9 @@ def Checkout(request):
         order.paid = True
         order.amount_paid = cart.total_cost()
         order.save()
+
+        decrement_product_quantity(order)
+        send_order_confirmation(order)
 
         cart.clear()
 
@@ -102,7 +154,7 @@ def add_to_cart(request):
 def delete_from_cart(request):
     data = json.loads(request.body)
     jsonresponse = {'success': True}
-    product_id = str(data['product_id'])
+    product_id = data['product_id']
 
     cart = Cart(request)
     cart.delete(product_id)
