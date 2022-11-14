719bdc08fb973fd6953b9047822e928fec9279af
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-11-14 13:48:09 +0000

    add: order confirmation, featured categories, popular products & recently view products to the core views

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
-from apps.store.models import Product
+from apps.store.models import Product, Category
+from apps.order.models import Order
+
+
+# confirmation email view
+def order_confirmation(request):
+    order = Order.objects.get(pk=1)
+    return render(request, 'order_confirmation.html', {'order': order})
 
 
 # Home view
 def home(request):
-    # only featured products are shown on the homepage
+    # only featured, popoular and recently viewed products are shown on the homepage
     products = Product.objects.filter(featured=True)
+    featured_categories = Category.objects.filter(is_featured=True)
+    popular_products = Product.objects.all().order_by('-num_visits')[0:4]
+    recently_viewed_products = Product.objects.all().order_by('-last_visit')[0:4]
 
     context = {
-        'products': products
+        'products': products,
+        'featured_categories': featured_categories,
+        'popular_products': popular_products,
+        'recently_viewed_products': recently_viewed_products
     }
 
     return render(request, 'home.html', context)
