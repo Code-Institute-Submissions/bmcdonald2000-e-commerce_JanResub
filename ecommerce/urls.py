cd587c16611af2801f05566eafa89f324f560906
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-11-14 19:59:28 +0000

    config: URL

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,29 +20,56 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from apps.cart.views import cart, success
-from apps.core.views import home, contact, about_us
-from apps.store.views import view_product, category_details
+from apps.cart.webhook import webhook
+from apps.cart.views import cart_details, success, fail
+from apps.core.views import home, contact, about_us, order_confirmation
+from apps.newsletter.api import api_add_subscriber
+from apps.order.views import admin_order_pdf
+from apps.store.views import view_product, category_details, search
+from apps.userprofile.views import signup, myaccount
 
-from apps.store.api import add_to_cart, delete_from_cart, Checkout , checkout_session
+
+from apps.coupon.api import valid_coupon
+from apps.store.api import add_to_cart, delete_from_cart
+from apps.store.api import checkout_session
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.auth import views
+
+from .sitemaps import StaticViewSitemap, CategorySitemap, ProductSitemap
+
+sitemaps = {'static': StaticViewSitemap, 'product': ProductSitemap, 'category': CategorySitemap}
 
 # url configarations for apps
 urlpatterns = [
-    path('api/create_checkout_session/', checkout_session, name='checkout_session'),
+    path('admin/order_pdf/<int:order_id>/', admin_order_pdf, name='admin_order_pdf'),
     path('admin/', admin.site.urls),
-    path('cart/', cart, name='cart'),
-    path('cart/success', success, name='success'),
+    path('search/', search, name='search'),
+    path('order_confirmation/', order_confirmation, name='order_confirmation'),
+    path('webhooks/', webhook, name='hook'),
+    path('cart/', cart_details, name='cart'),
+    path('cart/success/', success, name='success'),
+    path('cart/cancel/', fail.as_view(), name='cancel'),
     path('', home, name='home'),
     path('Contact_Us/', contact, name='contact'),
     path('About_Us/', about_us, name='about_us'),
 
-    # Store Paths
-    path('<slug:category_slug>/<slug:slug>/View_Details/', view_product, name='view_product'),
-    path('<slug:slug>/View_Details/', category_details, name='category_details'),
+    # user paths
+    path('myaccount/', myaccount, name='myaccount'),
+    path('signup/', signup, name='signup'),
+    path('login/', views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 
     # API paths
     path('api/add_to_cart/', add_to_cart, name='add'),
     path('api/delete_from_cart/', delete_from_cart, name='delete'),
-    path('api/checkout/', Checkout, name='checkout_api'),
+    path('api/checkout_session/', checkout_session, name='checkout_session'),
+    path('api/add_subscriber/', api_add_subscriber, name='api_add_subscriber'),
+    path('api/valid_coupon/', valid_coupon, name='valid_coupon'),
+
+    # Store Paths
+    path('<slug:category_slug>/<slug:slug>/', view_product, name='view_product'),
+    path('<slug:slug>/', category_details, name='category_details'),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
