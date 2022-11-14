7bd2921adb98ebb800bd51bc65e991c7a56bc9db
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-11-14 18:05:00 +0000

    add: user profile and signup forms

---
+++ apps/userprofile/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Userprofile
+
+
+# form for user profile
+class UserprofileForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(UserprofileForm, self).__init__(*args, **kwargs)
+
+        self.fields['address'].widget.attrs['class'] = 'input'
+        self.fields['postcode'].widget.attrs['class'] = 'input'
+        self.fields['city'].widget.attrs['class'] = 'input'
+        self.fields['phone'].widget.attrs['class'] = 'input'
+
+    class Meta:
+        model = Userprofile
+        fields = '__all__'
+        exclude = ('user',)
+
+
+# user sign up form
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=50, required=True)
+    last_name = forms.CharField(max_length=50, required=True)
+    email = forms.EmailField(max_length=255, required=True)
+
+    # function to improve signup form asethetic
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'input'
+        self.fields['email'].widget.attrs['class'] = 'input'
+        self.fields['password1'].widget.attrs['class'] = 'input'
+        self.fields['password2'].widget.attrs['class'] = 'input'
+        self.fields['first_name'].widget.attrs['class'] = 'input'
+        self.fields['last_name'].widget.attrs['class'] = 'input'
+
+    # meta data for signup form
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
