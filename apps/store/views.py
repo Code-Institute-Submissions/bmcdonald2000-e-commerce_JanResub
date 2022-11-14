d7f12e6c8a0dd02df24853a418ee2ed055f525eb
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-11-14 17:46:35 +0000

    feature: advanced search

--- apps/store/views.py
+++ apps/store/views.py
@@ -1,22 +1,52 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product, Category
+import random
+from datetime import datetime
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product, Category, ProductReview
+from django.db.models import Q
+from apps.cart.cart import Cart
 
 
-# producdetailsview.
+# search  function
+def search(request):
+    query = request.GET.get('query')
 
+    instock = request.GET.get('instock')
+    price_from = request.GET.get('price_from', 0)
+    price_to = request.GET.get('price_to', 100000)
+    sorting = request.GET.get('sorting', '-date_added')
+    products = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query)).filter(price__gte=price_from).filter(price__lte=price_to)
+
+    if instock:
+        products = products.filter(num_available__gte=1)
+
+    context = {
+
+        'query': query,
+        'products': products.order_by(sorting),
+        'instock': instock,
+        'price_from': price_from,
+        'price_to': price_to,
+        'sorting': sorting
+    }
+
+    return render(request, 'searchbar.html', context)
+
+
+# view product page functions
 def view_product(request, category_slug, slug):
     product = get_object_or_404(Product, slug=slug)
 
     context = {
-        'product': product
+        'product': product,
     }
 
     return render(request, 'view_product.html', context)
 
 
+# category view function
 def category_details(request, slug):
     category = get_object_or_404(Category, slug=slug)
-    products = category.products.all()
+    products = category.products.filter(parent=None)
     context = {
         'category': category,
         'products': products
