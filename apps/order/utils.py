1b5c92d6ed9251f2788ecf1471fa73ce66e1ea8b
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-11-14 14:01:28 +0000

    add: phone number to orders

--- apps/order/utils.py
+++ apps/order/utils.py
@@ -7,8 +7,8 @@ from apps.cart.cart import Cart
 from apps.order.models import Order, Item
 
 
-def checkout(request, first_name, last_name, email, address, postcode, city):
-    order = Order(first_name=first_name, last_name=last_name, email=email, address=address, postcode=postcode, city=city,)
+def checkout(request, first_name, last_name, email, address, postcode, city, phone):
+    order = Order(first_name=first_name, last_name=last_name, email=email, address=address, postcode=postcode, city=city, phone=phone)
 
     if request.user.is_authenticated:
         order.user = request.user
