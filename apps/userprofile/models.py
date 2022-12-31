ac2337fd9440c00da691285dbec25c1a6ed59a0d
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-12-31 20:18:23 +0000

    update: userprfile field lengths

--- apps/userprofile/models.py
+++ apps/userprofile/models.py
@@ -6,9 +6,9 @@ from django.db import models
 class Userprofile(models.Model):
     user = models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE)
     address = models.CharField(max_length=255, blank=True, null=True)
-    postcode = models.CharField(max_length=255, blank=True, null=True)
-    city = models.CharField(max_length=255, blank=True, null=True)
-    phone = models.CharField(max_length=255, blank=True, null=True)
+    postcode = models.CharField(max_length=8, blank=True, null=True)
+    city = models.TextField(max_length=15, blank=True, null=True)
+    phone = models.CharField(max_length=11, blank=True, null=True)
 
     def __str__(self):
         return '%s' % self.user.username
