db80307b6856c473da8c1bdd27d3e764f0b81743
Brieanna  McDonald <bmcdonald1406@gmail.com>
2023-01-01 00:35:27 +0000

    turn debug off

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -59,7 +59,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # 3rd party package is handling hosting
 ALLOWED_HOSTS = ["*"]
