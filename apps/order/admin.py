a4f501bb69f8524c129a0602f8c5d848f93e9f49
Brieanna  McDonald <bmcdonald1406@gmail.com>
2023-01-02 00:53:12 +0000

    fix: typo in short description

--- apps/order/admin.py
+++ apps/order/admin.py
@@ -22,7 +22,7 @@ def order_pdf(obj):
     return mark_safe('<a href="{}">PDF</a>'.format(reverse('admin_order_pdf', args=[obj.id])))
 
 
-order_name.short_description = 'PDF'
+order_pdf.short_description = 'PDF'
 
 
 # Function to change order status
