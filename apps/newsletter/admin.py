3de2c7227a4ba422ebde816850977a23010cc238
Brieanna  McDonald <bmcdonald1406@gmail.com>
2023-01-03 21:08:53 +0000

    register newsletter models in admin.py

--- apps/newsletter/admin.py
+++ apps/newsletter/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-from .models import Subscriber
+from .models import Subscriber, Newsletter
 
 
-# Registers the subscriber models
+# function to send newsletter
+def send_newsletter(modeladmin, request, queryset):
+    for newsletter in queryset:
+        newsletter.send(request)
+
+
+send_newsletter.short_description = "Send Selected Newsletter to Subscribers"
+
+
+# adds send newsletter button to admin
+class NewsletterAdmin(admin.ModelAdmin):
+    actions = [send_newsletter]
+
+
+# Registers the subscriber and newsletter models in the admin
 admin.site.register(Subscriber)
+admin.site.register(Newsletter, NewsletterAdmin)
