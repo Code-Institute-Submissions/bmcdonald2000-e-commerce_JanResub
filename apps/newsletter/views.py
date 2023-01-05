eabdcee88643b65674cc952e402cb5106ee0516a
Brieanna  McDonald <bmcdonald1406@gmail.com>
2023-01-05 02:39:03 +0000

    update: newsletter link

--- apps/newsletter/views.py
+++ apps/newsletter/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 from .models import Subscriber
@@ -23,36 +22,15 @@ def new(request):
         message = Mail(
             from_email=settings.DEFAULT_FROM_EMAIL,
             to_emails=sub.email,
-            subject='Newsletter Confirmation',
-            html_content='Welcome to the E-store newsletter! \
-                To complete the process \
-                <a href="https://tranquil-temple-81228.herokuapp.com/confirm/?email={}&conf_num={}"> click here to \
-                confirm your registration</a>.'.format(request.build_absolute_uri('https://tranquil-temple-81228.herokuapp.com/confirm/'),
-                                                    sub.email,
-                                                    sub.conf_num))
+            subject='Welcome to the E-store newsletter!',
+            html_content=' If you would like to be removed from the mailing list please\
+                <a href="https://tranquil-temple-81228.herokuapp.com/Contact_Us/"> \
+                click here to contact the Estore Team</a>.')
         sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
         response = sg.send(message)
-        return render(request, 'parts/newsletter.html', {'email': sub.email, 'action': 'added', 'form': SubscriberForm()}) 
+        return render(request, 'parts/newsletter.html', {'email': sub.email,
+                                                         'action': 'added',
+                                                         'form': SubscriberForm()})
     else:
-        return render(request, 'parts/newsletter.html', {'form': SubscriberForm()})
-
-
-# funciton to confirm subscription
-def confirm(request):
-    sub = Subscriber.objects.get(email=request.GET['email'])
-    if sub.conf_num == request.GET['conf_num']:
-        sub.confirmed = True
-        sub.save()
-        return render(request, 'parts/newsletter.html', {'email': sub.email, 'action': 'confirmed'})
-    else:
-        return render(request, 'parts/newsletter.html', {'email': sub.email, 'action': 'denied'})
-
-
-# fuction to unsubscribe
-def delete(request):
-    sub = Subscriber.objects.get(email=request.GET['email'])
-    if sub.conf_num == request.GET['conf_num']:
-        sub.delete()
-        return render(request, 'parts/newsletter.html', {'email': sub.email, 'action': 'unsubscribed'})
-    else:
-        return render(request, 'parts/newsletter.html', {'email': sub.email, 'action': 'denied'})
+        return render(request, 'parts/newsletter.html',
+                      {'form': SubscriberForm()})
