4f765ee520b6428df6c9a41b796ddb1b1c90a577
Brieanna  McDonald <bmcdonald1406@gmail.com>
2023-01-05 02:40:33 +0000

    fix: errors

--- apps/newsletter/models.py
+++ apps/newsletter/models.py
@@ -8,14 +8,15 @@ from sendgrid.helpers.mail import Mail
 # Subscriber model
 class Subscriber(models.Model):
     # subscriber email, date and confirmation details are stored
+
     email = models.EmailField(max_length=255)
     date_added = models.DateTimeField(auto_now_add=True)
     conf_num = models.CharField(max_length=15)
-    confirmed = models.BooleanField(default=False)
+    confirmed = models.BooleanField(default=True)
 
     # email and confirmation details are retruned as string
     def __str__(self):
-        return self.email + " (" + ("not " if not self.confirmed else "") + "confirmed)"
+        return self.email + " (" + ("not " if not self.confirmed else "") + "subscribed)"
 
 
 # Newsletter model so that the admin can send newsletters from the admin
@@ -30,7 +31,7 @@ class Newsletter(models.Model):
         return self.subject + " " + self.created_at.strftime("%B %d, %Y")
 
     # function to send newsletter to users
-    def send(self, request):
+    def send(self):
         contents = self.contents
         subscribers = Subscriber.objects.filter(confirmed=True)
         sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
@@ -39,9 +40,5 @@ class Newsletter(models.Model):
                     from_email=settings.DEFAULT_FROM_EMAIL,
                     to_emails=sub.email,
                     subject=self.subject,
-                    html_content=contents + (
-                        '<br><a href="https://tranquil-temple-81228.herokuapp.com/delete/?email={subscriber.email}&conf_num={subscriber.conf_num}">Unsubscribe</a>.').format(
-                            request.build_absolute_uri('/delete/'),
-                            sub.email,
-                            sub.conf_num))
+                    html_content=contents)
             sg.send(message)
